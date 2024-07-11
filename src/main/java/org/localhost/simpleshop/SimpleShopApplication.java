22f9c3e1a7d0641b840a87ead76e97c20c3d1bfa
marski84 <marski705@gmail.com>
2024-07-11 14:04:59 +0200

    first commit

---
+++ src/main/java/org/localhost/simpleshop/SimpleShopApplication.java
@@ -0,0 +1,13 @@
+package org.localhost.simpleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleShopApplication.class, args);
+    }
+
+}
